b4f186a0ce9fed9b90e03f0820b56bae7e5a10fa
Callum Wellwood-Kane <cwellkane@gmail.com>
2021-11-22 15:08:00 +1100

    created scss file

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-import "./styles/style.css";
+import "./styles/style.scss";
 import * as Three from "three";
+
+function throttled(delay, fn) {
+  let lastCall = 0;
+  return function (...args) {
+    const now = new Date().getTime();
+    if (now - lastCall < delay) {
+      return;
+    }
+    lastCall = now;
+    return fn(...args);
+  };
+}
 
 const scene = new Three.Scene();
 const camera = new Three.PerspectiveCamera(
@@ -21,7 +33,7 @@ renderer.render(scene, camera);
 const geometry = new Three.TorusGeometry(10, 3, 16, 100);
 const material = new Three.MeshStandardMaterial({ color: 0x000000 });
 const torus = new Three.Mesh(geometry, material);
-scene.add(torus);
+// scene.add(torus);
 
 const pointLight = new Three.PointLight(0xffffff);
 pointLight.position.set(20, 20, 20);
@@ -32,7 +44,7 @@ scene.add(pointLight, ambientLight);
 
 const lightHelper = new Three.PointLightHelper(pointLight);
 const gridHelper = new Three.GridHelper(200, 50);
-scene.add(gridHelper, lightHelper);
+// scene.add(gridHelper, lightHelper);
 
 // const controls = new OrbitControls(camera, renderer.domElement);
 
@@ -49,7 +61,7 @@ function addStar() {
 
 Array(200).fill().forEach(addStar);
 
-const spaceTexture = new Three.TextureLoader().load("space.jpg");
+const spaceTexture = new Three.TextureLoader().load("./images/space-2.png");
 scene.background = spaceTexture;
 
 const callumTexture = new Three.TextureLoader().load("callum.png");
@@ -59,7 +71,7 @@ const callum = new Three.Mesh(
   new Three.MeshBasicMaterial({ map: callumTexture })
 );
 
-scene.add(callum);
+// scene.add(callum);
 
 const marsTexture = new Three.TextureLoader().load("2k_mars.jpg");
 const mars = new Three.Mesh(
@@ -67,22 +79,39 @@ const mars = new Three.Mesh(
   new Three.MeshBasicMaterial({ map: marsTexture })
 );
 
-mars.position.z = 10;
+mars.position.z = 30;
 mars.position.setX(-10);
 
-document.body.onscroll = () => {
-  const t = document.body.getBoundingClientRect().top;
-  mars.rotation.x += 0.05;
-  mars.rotation.y += 0.075;
-  mars.rotation.z += 0.05;
+let t = 0;
+let prev = 0;
+let cup = 0;
+let cdown = 0;
+function scrollFunc() {
+  t = document.body.getBoundingClientRect().top;
+  console.log(t, cup, cdown);
+  if (t <= prev) {
+    mars.rotation.x += 0.05;
+    mars.rotation.y += 0.075;
+    cdown += 1;
+    mars.rotation.z += 0.05;
 
-  callum.rotation.y += 0.01;
-  callum.rotation.z += 0.01;
+    camera.position.z += t * -0.01;
+    camera.position.x += t * -0.0002;
+    camera.position.y += t * -0.0002;
+  } else {
+    mars.rotation.x += -0.05;
+    mars.rotation.y += -0.075;
+    cup += 1;
+    mars.rotation.z += -0.05;
 
-  camera.position.z = t * -0.01;
-  camera.position.x = t * -0.0002;
-  camera.position.y = t * -0.0002;
-};
+    camera.position.z += t * 0.01;
+    camera.position.x += t * 0.0002;
+    camera.position.y += t * 0.0002;
+  }
+  prev = t;
+}
+document.addEventListener("scroll", throttled(10, scrollFunc));
+
 scene.add(mars);
 function animate() {
   requestAnimationFrame(animate);
